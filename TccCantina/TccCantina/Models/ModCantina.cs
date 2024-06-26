817020ebcd0e3f12868c0d670d9096bf37ec31f6
leonardo <leozinhogameplays156@gmail.com>
2024-06-25 22:04:11 -0300

    Banco de dados SqLite

--- TccCantina/TccCantina/Models/ModCantina.cs
+++ TccCantina/TccCantina/Models/ModCantina.cs
@@ -1,63 +1,56 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-using MySqlConnector;
-using SkiaSharp;
+﻿using SQLite;
+using System;
 
 namespace TccCantina.Models
 {
+    [Table("Usuarios")]
     public class ModCantina
     {
         public int IdUsuario { get; set; }
-        public String Nome { get; set; }
-        public String Cpf { get; set; }
-        public String Email { get; set; }
-        public String Senha { get; set; }
+        public string Nome { get; set; }
+        public string Cpf { get; set; }
+        public string Email { get; set; }
+        public string Senha { get; set; }
         public int Matricula { get; set; }
-        public String Curso { get; set; }
-        public String Tipo { get; set; }
+        public string Curso { get; set; }
+        public string Tipo { get; set; }
         public byte[] Foto { get; set; }
-        public String Telefone { get; set; }
+        public string Telefone { get; set; }
     }
 
+    [Table("Professores")]
     public class Professores
     {
         public int IdProfessores { get; set; }
-        public String Nome { get; set; }
+        public string Nome { get; set; }
         public int Matricula { get; set; }
     }
 
+    [Table("Produtos")]
     public class Produtos
     {
         public int IdProdutos { get; set; }
-        public String Nome { get; set; }
-        public String Descricao { get; set; }
-        public Double Valor { get; set; }
-        public String Tipo { get; set; }
+        public string Nome { get; set; }
+        public string Descricao { get; set; }
+        public double Valor { get; set; }
+        public string Tipo { get; set; }
         public byte[] Foto { get; set; }
     }
 
-    public class Carrinho
-    {
-        public int IdCarrinho { get; set; }
-        public int IdUsuario { get; set; }
-        public int IdProduto { get; set; }
-        public int Quantidade { get; set; }
-        public byte[] Foto { get; set; }
-    }
-
+    [Table("Carrinho")]
     public class CarrinhoFiltrado
     {
         public int IdCarrinhoF { get; set; }
-        public String Nome { get; set; }
+        public string Nome { get; set; }
         public int Quantidade { get; set; }
-        public Double Valor { get; set; }
+        public double Valor { get; set; }
         public int IdCarrinho { get; set; }
     }
 
+    [Table("ValorTotal")]
     public class TotalCarrinho
     {
-        int IdTotalCarrinho { get; set; }
-        public Double ValorTotal { get; set; }
+        public int IdTotalCarrinho { get; set; }
+        public double ValorTotal { get; set; }
     }
 }
